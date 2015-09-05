b73405b4dd5306b3f1f715427fa496d6b65cc058
dream-365 <support-jec@hotmail.com>
2015-09-05 13:49:47 +0800

    update the category

--- Toolkits/EasyAnalysis/Scripts/data.js
+++ Toolkits/EasyAnalysis/Scripts/data.js
@@ -1,10 +1,57 @@
 ﻿var _global_data = {
     categories: [
       { index: 0, name: 'Windows Runtime' },
-      { index: 1, name: 'XAML UI' }
+      { index: 1, name: 'Visual Studio' },
+      { index: 2, name: 'Devices' },
+      { index: 3, name: 'XAML' },
+      { index: 4, name: 'WINJS' },
+      { index: 5, name: 'DirectX' },
+      { index: 6, name: 'Third Party Ecosystem' }
     ],
     typeGroups: [
-        [{ id: 1, name: 'Background Task' }, { id: 2, name: 'Network' }],
-        [{ id: 3, name: 'Controls' }, { id: 4, name: 'Styles' }]
+        // Windows Runtime
+        [
+            { id: 1, name: 'Background Task' },
+            { id: 2, name: 'Network' },
+            { id: 3, name: 'Media' },
+            { id: 4, name: 'File Access'},
+            { id: 5, name: 'Others'}
+        ],
+        // Visual Studio
+        [
+            { id: 10, name: 'Emulator' },
+            { id: 11, name: 'XAML Designer' }
+        ],
+        // Devices
+        [
+            { id: 20, name: 'Bluetooth' },
+            { id: 21, name: 'Wifi-Direct' },
+            { id: 22, name: 'Accelerator' },
+            { id: 23, name: 'Scanner' },
+            { id: 24, name: 'NFC' },
+            { id: 25, name: 'MIDI' },
+            { id: 26, name: 'Smart Card' }
+        ],
+        // XAML
+        [
+            { id: 30, name: 'Controls' },
+            { id: 31, name: 'Layout' },
+            { id: 32, name: 'Data Binding' }
+        ],
+        // WINJS
+        [
+            { id: 40, name: 'JavaScript' },
+            { id: 41, name: 'Controls' }
+        ],
+        // DirectX
+        [
+            { id: 50, name: '3D' },
+            { id: 51, name: '2D' }
+        ],
+        // Third Party Ecosystem
+        [
+            { id: 60, name: 'Unity' },
+            { id: 61, name: 'Apache Cordova' }
+        ]
     ]
 };
