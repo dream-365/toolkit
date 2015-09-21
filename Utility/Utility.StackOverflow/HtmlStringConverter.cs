3a19567a186e74bb365edc3ef55a7a4b2bcd9d13
dream-365 <support-jec@hotmail.com>
2015-09-21 13:28:50 +0800

    add stackoverflow thread query api

---
+++ Utility/Utility.StackOverflow/HtmlStringConverter.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Utility.StackOverflow
+{
+    public class HtmlStringConverter : JsonConverter
+    {
+        public override bool CanConvert(Type objectType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            return HttpUtility.HtmlDecode(reader.Value.ToString());
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            writer.WriteValue(HttpUtility.HtmlEncode(value));
+        }
+    }
+}
---
+++ Utility/Utility.StackOverflow/SimpleJavascriptDateTimeConverter.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json.Converters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Utility.StackOverflow
+{
+    public class SimpleJavascriptDateTimeConverter : DateTimeConverterBase
+    {
+        private const long InitialJavaScriptDateTicks = 621355968000000000;
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            long ticks = (long)reader.Value;
+
+            DateTime dateTime = new DateTime((ticks * 10000000) + InitialJavaScriptDateTicks, DateTimeKind.Utc);
+
+            return dateTime;
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            throw new NotFiniteNumberException();
+        }
+    }
+}
