420514ce6967473c43461f8d0cf5ee7022ce36f1
dream-365 <support-jec@hotmail.com>
2015-10-28 10:43:09 +0800

    add missing sql

---
+++ Toolkits/EasyAnalysis.Backend/Query/get_thread_profile.sql
@@ -0,0 +1,8 @@
+﻿SELECT RTRIM([Id]) AS [Id]
+      ,[Title]
+      ,[CreateOn]
+      ,[Category]
+      ,[Type]
+      ,[Repository]
+  FROM [uwpdb].[dbo].[VwThreads]
+  WHERE [Repository] = @repository AND [CreateOn] BETWEEN @start and @end
---
+++ Toolkits/EasyAnalysis.Backend/Query/get_thread_tags.sql
@@ -0,0 +1,5 @@
+﻿SELECT [Tags].[Name]
+FROM [uwpdb].[dbo].[ThreadTags] [ThreadTags]
+  INNER JOIN [uwpdb].[dbo].[Tags] [Tags]
+  ON [ThreadTags].[TagId] = [Tags].[Id]
+WHERE [ThreadId] = @ThreadId
