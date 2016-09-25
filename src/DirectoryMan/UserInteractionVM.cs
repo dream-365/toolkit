c6a19596890681a8e64b4129a1eeb2a669bbbfc2
Jeffrey Chen <support-jec@hotmail.com>
2016-09-25 13:16:05 +0800

    code simplify

--- src/DirectoryMan/UserInteractionVM.cs
+++ src/DirectoryMan/UserInteractionVM.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
 using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DirectoryMan
 {
@@ -94,10 +90,7 @@ namespace DirectoryMan
 
         private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
         {
-            if (PropertyChanged != null)
-            {
-                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
-            }
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
